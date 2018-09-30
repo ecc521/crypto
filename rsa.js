0b12bb5dfb960400f5cdb31000c222ad7b512c38
ecc521 <admin@rivers.run>
2018-09-30 18:14:38 -0400

    Update rsa.js

--- rsa.js
+++ rsa.js
@@ -1,7 +1,16 @@
 //RSA.js JavaScript Function Library
+//Beta release
+
 //Copyright (©) 2018 Tucker Willenborg
-//Version 0.62
-//License is located at ecc521.github.io/crypto/license.txt
+
+//Permission is hereby granted, free of charge, to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of this software, subject to the following conditions:
+
+//1. This license shall be included in all copies or substantial portions of this software.
+//2. Copies or substantial portions of this software may not be sold alone.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
 
 
 Tucker = window.Tucker || {};
