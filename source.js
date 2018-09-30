19ca2a270394fb128b8d0b123337dbd2c9c87761
ecc521 <admin@rivers.run>
2018-09-30 18:15:45 -0400

    Update source.js

--- source.js
+++ source.js
@@ -1,6 +1,13 @@
 //Tucker.js JavaScript Function Library
+
 //Copyright (©) 2018 Tucker Willenborg
-//License is located at ecc521.github.io/crypto/license.txt
+
+//Permission is hereby granted, free of charge, to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of this software, subject to the following conditions:
+
+//1. This permission notice, the above copyright notice, and the below disclaimer shall be included in all copies or substantial portions of this software.
+//2. Copies or substantial portions of this software may not be sold alone.
+
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 
 Tucker = window.Tucker || {};
