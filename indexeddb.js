260ca7ba9cedf6edd917e88bd35228cd070c9aec
ecc521 <42424091+ecc521@users.noreply.github.com>
2018-11-04 13:14:44 -0500

    Update indexeddb.js

--- indexeddb.js
+++ indexeddb.js
@@ -10,10 +10,67 @@ let loader = function(appendto){
         throw new Error("You must pass an object as parameter number 1. The methods will be added to that object.")
     }
     
-    let _db; //We can only declare private variables at objects creation
     
-    appendto.loaddb = async function(dbname){
-        _db = await new Promise(function(resolve,reject){
+    
+    function database(_db) {
+        this.name = _db.name
+        
+        this.set = function(key, value) {
+            let trans = _db.transaction("data", "readwrite")
+            let store = trans.objectStore("data");
+
+            //It's an objectStore, so lets make an object
+            let obj = {key:key,value:value}
+
+            let request = store.put(obj);
+
+            return new Promise(function(resolve, reject){
+                request.onsuccess = resolve
+                request.onerror = reject
+            }) 
+        }
+        
+        this.get = function(key) {
+            let trans = _db.transaction("data", "readonly")
+            let store = trans.objectStore("data");
+
+            let request = store.get(key)
+
+            return new Promise(function(resolve, reject){
+
+                request.onsuccess = function(){
+                    //Check that the value exists
+                    if(request.result) {
+                        resolve(request.result.value)   
+                    }
+                    else {resolve(null)} //Return null if doesn't exist
+                }
+
+                request.onerror = reject
+            }) 
+        }
+        
+        this.delete = function(key) {
+            let trans = _db.transaction("data", "readwrite")
+            let store = trans.objectStore("data");
+
+            let request = store.delete(key)
+
+
+            return new Promise(function(resolve, reject){
+                request.onsuccess = resolve
+                request.onerror = reject
+            }) 
+        }
+    }
+    
+    
+    
+    
+    
+    
+    appendto.loaddb = function(dbname){
+        return new Promise(function(resolve,reject){
         
             let store = indexedDB.open(dbname, 1);   
 
@@ -23,10 +80,10 @@ let loader = function(appendto){
             }
 
             store.onsuccess = function(){
-                resolve(store.result)
+                resolve(new database(store.result))
             }
         
-        })          
+        }) 
     }
     
     appendto.deletedb = async function(dbname){
@@ -38,54 +95,6 @@ let loader = function(appendto){
         })
     }
     
-    
-    appendto.setitem = async function(key, value) {
-        let trans = _db.transaction("data", "readwrite")
-        let store = trans.objectStore("data");
-        
-        //It's an objectStore, so lets make an object
-        let obj = {key:key,value:value}
-        
-        let request = store.put(obj);
-        
-        return new Promise(function(resolve, reject){
-            request.onsuccess = resolve
-            request.onerror = reject
-        }) 
-    }
-    
-    appendto.getitem = async function(key) {
-        let trans = _db.transaction("data", "readonly")
-        let store = trans.objectStore("data");
-        
-        let request = store.get(key)
-        
-        return new Promise(function(resolve, reject){
-            
-            request.onsuccess = function(){
-                //Check that the value exists
-                if(request.result) {
-                    resolve(request.result.value)   
-                }
-                else {resolve(null)} //Return null if doesn't exist
-            }
-            
-            request.onerror = reject
-        }) 
-    }
-    
-    appendto.deleteitem = async function(key) {
-        let trans = _db.transaction("data", "readwrite")
-        let store = trans.objectStore("data");
-        
-        let request = store.delete(key)
-        
-        
-        return new Promise(function(resolve, reject){
-            request.onsuccess = resolve
-            request.onerror = reject
-        }) 
-    }
     
     
     //Library cloning function
