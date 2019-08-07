6ae9907d9516c61bea39a1196f0ad9d34143b721
ecc521 <admin@rivers.run>
2019-08-07 13:04:55 -0400

    Update indexeddb.js
    Bug fix and add copyright

--- indexeddb.js
+++ indexeddb.js
@@ -1,3 +1,9 @@
+//indexeddb.js JavaScript Function Library
+//https://github.com/ecc521/librarys/blob/master/indexeddb.js
+
+//Copyright (©) 2019 Tucker Willenborg, MIT License
+
+
 (function() {
 
 //We need to defind loader so that appendto.new can be set.
@@ -6,12 +12,12 @@ let loader = function(appendto){
     if (!appendto || typeof appendto !== "object") {
         throw new Error("You must pass an object as parameter number 1. The methods will be added to that object.")
     }
-    
-    
-    
+
+
+
     function database(_db) {
         this.name = _db.name
-        
+
         this.set = function(key, value) {
             let trans = _db.transaction("data", "readwrite")
             let store = trans.objectStore("data");
@@ -24,9 +30,9 @@ let loader = function(appendto){
             return new Promise(function(resolve, reject){
                 request.onsuccess = resolve
                 request.onerror = reject
-            }) 
+            })
         }
-        
+
         this.get = function(key) {
             let trans = _db.transaction("data", "readonly")
             let store = trans.objectStore("data");
@@ -38,15 +44,15 @@ let loader = function(appendto){
                 request.onsuccess = function(){
                     //Check that the value exists
                     if(request.result) {
-                        resolve(request.result.value)   
+                        resolve(request.result.value)
                     }
                     else {resolve(null)} //Return null if doesn't exist
                 }
 
                 request.onerror = reject
-            }) 
+            })
         }
-        
+
         this.delete = function(key) {
             let trans = _db.transaction("data", "readwrite")
             let store = trans.objectStore("data");
@@ -57,21 +63,21 @@ let loader = function(appendto){
             return new Promise(function(resolve, reject){
                 request.onsuccess = resolve
                 request.onerror = reject
-            }) 
+            })
         }
     }
-    
-    
-    
-    
-    
-    
+
+
+
+
+
+
     appendto.loaddb = function(dbname){
         return new Promise(function(resolve,reject){
-        
-            let store = indexedDB.open(dbname, 1);   
 
-            store.onupgradeneeded = function(){
+            let store = indexedDB.open(dbname, 1);
+
+            store.onupgradeneeded = function(event){
                 let db = event.target.result;
                 db.createObjectStore("data", { keyPath: "key" });
             }
@@ -79,28 +85,28 @@ let loader = function(appendto){
             store.onsuccess = function(){
                 resolve(new database(store.result))
             }
-        
-        }) 
+
+        })
     }
-    
+
     appendto.deletedb = async function(dbname){
         let request = indexedDB.deleteDatabase(dbname);
-        
+
         return new Promise(function(resolve,reject){
             request.onsuccess = resolve
             request.onerror = reject
         })
     }
-    
-    
-    
+
+
+
     //Library cloning function
     if (!appendto._duplist) {
         appendto._duplist = []
     }
     //Add the loading function
     appendto._duplist.push(loader)
-    
+
     //If the user wants to duplicate the library, they call this.
     //It creates a new object, then calls the method creators on them
     appendto.duplicate = function() {
